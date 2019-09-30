eb3298cfd68b9856934503fce503992a59e40a7f
Bryon Harland <bryonlh@gmail.com>
2019-09-29 19:06:04 -0700

    updates to the controller

--- controllers/article_controller.js
+++ controllers/article_controller.js
@@ -46,7 +46,7 @@ router.get("/scrape", function (req, res) {
   });
 });
 
-router.get("/", (req, res) => {
+router.get("/", function (req, res) {
   db.Article.find({})
     .then(function (dbArticle) {
       // If we were able to successfully find Articles, send them back to the client
@@ -56,6 +56,22 @@ router.get("/", (req, res) => {
         articles: dbArticle
       };
       res.render("index", hbsObject);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+router.get("/saved", function (req, res) {
+  db.Article.find({ isSaved: true })
+    .then(function (retrievedArticles) {
+      // If we were able to successfully find Articles, send them back to the client
+      let hbsObject;
+      hbsObject = {
+        articles: retrievedArticles
+      };
+      res.render("saved", hbsObject);
     })
     .catch(function (err) {
       // If an error occurred, send it to the client
@@ -75,6 +91,30 @@ router.get("/articles", function (req, res) {
       // If an error occurred, send it to the client
       res.json(err);
       res.render("index", hbsObject);
+    });
+});
+
+router.put("/save/:id", function (req, res) {
+  db.Article.findOneAndUpdate({ _id: req.params.id }, { isSaved: true })
+    .then(function (data) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(data);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });;
+});
+
+router.put("/remove/:id", function (req, res) {
+  db.Article.findOneAndUpdate({ _id: req.params.id }, { isSaved: false })
+    .then(function (data) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(data)
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
     });
 });
 
@@ -103,6 +143,43 @@ router.post("/articles/:id", function (req, res) {
       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+    })
+    .then(function (dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+// Route for saving/updating an Article's associated Note
+router.post("/note/:id", function (req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.create(req.body)
+    .then(function (dbNote) {
+      // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+      // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+      // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+      return db.Article.findOneAndUpdate({ _id: req.params.id }, { $push: { note: dbNote._id } }, { new: true });
+    })
+    .then(function (dbArticle) {
+      // If we were able to successfully update an Article, send it back to the client
+      res.json(dbArticle);
+    })
+    .catch(function (err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+router.delete("/note/:id", function (req, res) {
+  // Create a new note and pass the req.body to the entry
+  db.Note.findByIdAndRemove({ _id: req.params.id })
+    .then(function (dbNote) {
+
+      return db.Article.findOneAndUpdate({ note: req.params.id }, { $pullAll: [{ note: req.params.id }] });
     })
     .then(function (dbArticle) {
       // If we were able to successfully update an Article, send it back to the client
